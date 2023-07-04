e1f3252f3e73959d965c47d04217e3e1a5ab1af9
Ritik Kumar <kumarritik9540@gmail.com>
2023-07-05 04:15:11 +0530

    CONNECT

--- src/Components/Header/Navbarlist.js
+++ src/Components/Header/Navbarlist.js
@@ -9,8 +9,14 @@ import navicon1 from '../../assets/img/nav-icon1.svg'
 import navicon2 from '../../assets/img/nav-icon2.svg'
 import navicon3 from '../../assets/img/nav-icon3.svg'
 import arrow1 from '../../assets/img/arrow1.svg'
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+// import { useNavigate } from 'react-router-dom';
 // import {ArrowRightCircle} from 'react-bootstrap'
 const Navbarlist = ()=>{
+    // const navigate = useNavigate()
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
      
@@ -40,7 +46,9 @@ const Navbarlist = ()=>{
         <Navbar.Brand href="#home">
             <img src={rk} alt='logo'/>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateLink('home')}>Home</Nav.Link>
@@ -49,11 +57,13 @@ const Navbarlist = ()=>{
           </Nav>
           <span className='navbar-text'>
             <div className='social-icon'>
-                <a href='#'><img src={navicon1} alt=''/></a>
-                <a href='#'><img src={navicon2} alt=''/></a>
-                <a href='#'><img src={navicon3} alt=''/></a>
+                <a href='#'><img src={navicon1} alt='linkedin'/></a>
+                <a href='#'><img src={navicon3} alt='instagram'/></a>
+                <a href='#'><img src={navicon2} alt='github'/></a>
             </div>
-            <button className='vvd'><span>Let's Connect</span></button>
+           
+            {/* <button className='vvd' ><span>Let's Connect</span></button> */}
+            <a href='#connect'><span>Let's Connect</span></a>
           </span>
         </Navbar.Collapse>
       </Container>
