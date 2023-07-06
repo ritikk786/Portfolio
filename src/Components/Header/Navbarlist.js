dacfa64636ea2d299235e591cc36a69bb5821354
Ritik Kumar <kumarritik9540@gmail.com>
2023-07-06 13:59:40 +0530

    copyright setup

--- src/Components/Header/Navbarlist.js
+++ src/Components/Header/Navbarlist.js
@@ -51,9 +51,9 @@ const Navbarlist = ()=>{
           </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateLink('skills')}>Skills</Nav.Link>
-            <Nav.Link href="#Projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={()=> onUpdateLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#home" className='navbar-link' onClick={()=> onUpdateLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className='navbar-link' onClick={()=> onUpdateLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#Projects" className='navbar-link' onClick={()=> onUpdateLink('projects')}>Projects</Nav.Link>
           </Nav>
           <span className='navbar-text'>
             <div className='social-icon'>
